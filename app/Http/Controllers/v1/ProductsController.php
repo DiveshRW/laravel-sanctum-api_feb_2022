a2d9af0918adb74aba7157dc6258babbedfa9bd1
Divesh <diveshk442@gmail.com>
2022-02-03 23:38:51 +0530

    3 feb 2022 Status

---
+++ app/Http/Controllers/v1/ProductsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductResource::collection(Product::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    $faker = \Faker\Factory::create(1);
+
+       $product = Product::create([
+        'name'=> $faker->name(),
+        'slug' => $faker->url(),
+        'description' => $faker->text(),
+        'price' => $faker->unique()->randomDigit
+    ]);
+    return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        // return new ProductResource($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+      $product->update([
+            'name'=> $request->input('name'),
+            'slug' =>  $request->input('slug'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+        ]);
+        return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response(null,204);
+    }
+}
